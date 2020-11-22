f2380d8fbcbe3a1db099dfa7999108531bf86c03
Petros Kataras <petroskataras@gmail.com>
2020-11-22 18:33:28 +0100

    Add Dockerfile for running headless on GPU instances.

---
+++ docker/entrypoint.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+node /home/Software/CiDocker/blocks/Cinder-GstWebRTC/web/server.js --binaryPath=/home/Software/CiDocker/blocks/Cinder-GstWebRTC/samples/BasicStreamApp/proj/cmake/build/Debug/BasicStreamApp/BasicStreamApp
